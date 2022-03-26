24cc4bfe8556d9a613e7a402dcb510360d51ade3
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-03-26 12:54:10 +0200

    added a try catch block in the server.js file

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8080
 
 try {
     server.listen(port)
-    console.log(`server up and running on port: ${port}`)
+    console.log(`server is up and running on port: ${port}`)
 } catch (err) {
     console.log(err)
-}
+s}
