bbda542f3ddccf0d767d2eaaca4e08520901523b
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-03-27 14:00:27 +0200

    done with the profile modal

--- client/src/components/profile-modal/Modal.jsx
+++ client/src/components/profile-modal/Modal.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { 
+    MdPerson, 
+    MdLocationOn, 
+    MdMenuBook, 
+    MdPhonelinkRing 
+} from "react-icons/md";
 
 import './Modal.css'
 
@@ -17,28 +23,40 @@ const Modal = ({ show, close }) => {
         </div>
         <div className="profile-modal-content">
             <div className="profile-modal-body">
-                <h4 className="profile-modal-h4">User profile</h4>
+                <h4 className="profile-modal-h4">Update your profile</h4>
                  <div className="profile-modal-form-folder">
                      <form action="#">
-                        <div className="profile-input-boxes">
-                            <div className="input-box">
-                                <input type="text" placeholder="enter your first name" />
-                            </div>
-                            <div className="input-box">
-                                <input type="text" placeholder="enter your second name" />
-                            </div>
-                            <div className="input-box">
-                                <input type="text" placeholder="enter your first name" />
-                            </div>
-                            <div className="input-box">
-                                <input type="text" placeholder="enter your first name" />
-                            </div>
+                        <div className="profile-form-content">
+                           <div className="profile-holder">
+                                <div className="profile-input-boxes">
+                                    <div className="input-box">
+                                        <MdPerson size={30} className="profile-icons" />
+                                        <input type="text" placeholder="enter your first name" />
+                                    </div>
+                                    <div className="input-box">
+                                        <MdPerson size={30} className="profile-icons" />
+                                        <input type="text" placeholder="enter your second name" />
+                                    </div>
+                                    <div className="input-box">
+                                        <MdLocationOn size={30} className="profile-icons" />
+                                        <input type="text" placeholder="enter your location" />
+                                    </div>
+                                    <div className="input-box">
+                                        <MdMenuBook size={30} className="profile-icons" />
+                                        <input type="text" placeholder="enter your tertiary institution" />
+                                    </div>
+                                    <div className="input-box">
+                                        <MdPhonelinkRing size={30} className="profile-icons" />
+                                        <input type="text" placeholder="enter your mobile number" />
+                                    </div>
+                                </div>
+                           </div>
                         </div>
                      </form>
                  </div>
             </div>
             <div className="profile-modal-footer">
-                <button onClick={close} className="profile-save-btn">Save</button>
+                <button onClick={close} className="profile-save-btn">Update information</button>
             </div>
         </div>
     </div>
