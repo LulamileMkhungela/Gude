e08c75d6c96675bf9a7cb1adf643c0a337d7d15a
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-04-13 16:20:23 +0200

    Modified side navigation files

--- client/src/components/side-nav/sideNav.jsx
+++ client/src/components/side-nav/sideNav.jsx
@@ -13,28 +13,40 @@ const SideNav = ({history}) => {
             </div>
             <ul className={'list-group'}>
                 <li className={'list-group-item'} onClick={()=>history.push('/electronics')}>
-                    <i className="material-icons devices">devices</i>
-                    <Link to={'/electronics'}>Electronics</Link>
+                    <Link to={'/electronics'}>
+                        <i className="material-icons devices">devices</i>
+                        Electronics
+                    </Link>
                 </li>
                 <li className={'list-group-item'} onClick={()=>history.push('/outdoors')}>
-                    <i className={'fas fa-campground'}> </i>
-                    <Link to={'/outdoors'}>Outdoors</Link>
+                    <Link to={'/outdoors'}>
+                        <i className={'fas fa-campground'}> </i>
+                        Outdoors
+                    </Link>
                 </li>
                 <li className={'list-group-item'}>
-                    <i className={'fab fa-xbox'}> </i>
-                    <Link to={''}>Gaming</Link>
+                    <Link to={''}>
+                        <i className={'fab fa-xbox'}> </i>
+                        Gaming
+                    </Link>
                 </li>
                 <li className={'list-group-item'}>
-                    <i className={'fab fa-freebsd'}> </i>
-                    <Link to={''}>Freebies</Link>
+                    <Link to={''}>
+                        <i className={'fab fa-freebsd'}> </i>
+                        Freebies
+                    </Link>
                 </li>
                 <li className={'list-group-item'}>
-                    <i className={'fa fa-tachometer'}> </i>
-                    <Link to={''}>Appliances</Link>
+                    <Link to={''}>
+                        <i className={'fa fa-tachometer'}> </i>
+                        Appliances
+                    </Link>
                 </li>
                 <li className={'list-group-item'}>
-                    <i className={'fa fa-book'}> </i>
-                    <Link to={''}>Stationery</Link>
+                    <Link to={''}>
+                        <i className={'fa fa-book'}> </i>
+                        Stationery
+                    </Link>
                 </li>
             </ul>
 
