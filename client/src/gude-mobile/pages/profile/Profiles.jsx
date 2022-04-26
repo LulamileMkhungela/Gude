f3771c76e8a31f537ecd938eb3597530a6f8f183
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-04-26 16:58:24 +0200

    Modified the mobile profile page

--- client/src/gude-mobile/pages/profile/Profiles.jsx
+++ client/src/gude-mobile/pages/profile/Profiles.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FaChevronLeft } from "react-icons/fa";
+import { MdSettings } from "react-icons/md";
 
 import './Profile.css'
 import img from '../../../images/default_img.png'
@@ -9,9 +10,10 @@ import MobileListing from './section/Listing/MobileListing'
 import MobileDraft from './section/Draft/MobileDraft'
 import MobileAbout from './section/About/MobileAbout'
 import MobileSold from './section/Sold/MobileSold'
+import Settings from './section/Settings/Settings'
 
 const Profiles = () => {
-  const [toggle, setToggle] = useState(1)
+  const [toggle, setToggle] = useState(0)
 
   return (
     <div>
@@ -34,6 +36,10 @@ const Profiles = () => {
       <div className="profile-inline-btn">
         <div className="profile-line">
           <div 
+            className={toggle === 0 ? "profile-spacing-active" : "profile-spacing"}
+            onClick={() => setToggle(0)}
+            ><MdSettings size={20} /></div>
+          <div 
             style={{marginLeft: 20}} 
             className={toggle === 1 ? "profile-spacing-active" : "profile-spacing"}
             onClick={() =>setToggle(1)}
@@ -54,9 +60,10 @@ const Profiles = () => {
             onClick={() =>setToggle(4)}
           >Sold</div>
         </div>
-        <hr />
       </div>
+      <div className="profilse-line"></div>
       <div className="profiles-screens">
+        {toggle === 0 && <Settings />}
         {toggle === 1 && <MobileListing />}
         {toggle === 2 && <MobileDraft />}  
         {toggle === 3 && <MobileAbout />}
