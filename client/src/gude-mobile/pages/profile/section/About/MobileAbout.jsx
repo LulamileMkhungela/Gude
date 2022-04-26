6786a24473c5a3b5599c97927ae733edc3f2998a
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-04-26 18:48:04 +0200

    modified mobile profile design

---
+++ client/src/gude-mobile/pages/profile/section/About/MobileAbout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MobileAbout = () => {
+  return (
+    <div>MobileAbout</div>
+  )
+}
+
+export default MobileAbout
---
+++ client/src/gude-mobile/pages/profile/section/Sold/MobileSold.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MobileSold = () => {
+  return (
+    <div>MobileSold</div>
+  )
+}
+
+export default MobileSold
