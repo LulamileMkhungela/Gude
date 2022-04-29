7112bbecfb687592c653e3bf7436fab76d61989c
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-04-29 09:56:29 +0200

    Done with profile

--- client/src/gude-mobile/pages/profile/section/Sold/MobileSold.jsx
+++ client/src/gude-mobile/pages/profile/section/Sold/MobileSold.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import './MobileSold.css'
+
 const MobileSold = () => {
   return (
-    <div>MobileSold</div>
+    <div className="mobile-sold-container">
+      <div className="mobile-sold-header">
+        <h1>Sold Products</h1> 
+      </div>
+      <div className="mobile-sold-items">
+        <p>a list of sold items</p>
+      </div>
+    </div>
   )
 }
 
