85aa0d2e997471685103ab5b1d839c25f343c5dd
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-04-29 09:27:48 +0200

    Modified mobile about and added mobile design css file

--- client/src/gude-mobile/pages/profile/section/About/MobileAbout.jsx
+++ client/src/gude-mobile/pages/profile/section/About/MobileAbout.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import { MdWork, MdHome, MdLocationOn } from "react-icons/md";
+import { FaGraduationCap } from "react-icons/fa";
+
+import './MobileAbout.css'
 
 const MobileAbout = () => {
   return (
-    <div>MobileAbout</div>
+    <div className="mobile-about-container">
+      <div className="mobile-about-intro">
+        <h1>Introduction</h1>
+      </div>
+      <div className="mobile-about-due">
+        <div className="mobile-icon-design">
+          <MdWork className="mobile-icon" />
+        </div>
+        <div className="mobile-paragraph-design">
+          <p>Data here</p>
+        </div>
+      </div>
+      <div className="mobile-about-due">
+        <div className="mobile-icon-design">
+          <FaGraduationCap className="mobile-icon" />
+        </div>
+        <div className="mobile-paragraph-design">
+          <p>Data here</p>
+        </div>
+      </div>
+      <div className="mobile-about-due">
+        <div className="mobile-icon-design">
+          <FaGraduationCap className="mobile-icon" />
+        </div>
+        <div className="mobile-paragraph-design">
+          <p>Data here</p>
+        </div>
+      </div>
+      <div className="mobile-about-due">
+        <div className="mobile-icon-design">
+          <MdHome className="mobile-icon" />
+        </div>
+        <div className="mobile-paragraph-design">
+          <p>Data here</p>
+        </div>
+      </div>
+      <div className="mobile-about-due">
+        <div className="mobile-icon-design">
+          <MdLocationOn className="mobile-icon" />
+        </div>
+        <div className="mobile-paragraph-design">
+          <p>Data here</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
